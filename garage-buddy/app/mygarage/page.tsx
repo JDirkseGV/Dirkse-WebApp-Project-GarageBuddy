244c2e1dd35b34d613f156139cde0aab73f35ea9
John <dirksej@mail.gvsu.edu>
2023-04-22 17:04:54 -0400

    Created a left side bar component and used the authentication session to display personal user information. Also implemented a quick log out button

--- garage-buddy/app/mygarage/page.tsx
+++ garage-buddy/app/mygarage/page.tsx
@@ -1,6 +1,10 @@
+import LeftBar from "@/components/LeftBar"
+
 export default function GaragePage() {
     return (
-        <p className="text-white px-4">Your Vehicles: </p>
-
+        <div className="flex">
+            <LeftBar />
+            <p className="text-white px-4">Your Vehicles: </p> 
+        </div>
     )
 }
---
+++ garage-buddy/components/LeftBar.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { signOut, useSession } from "next-auth/react"
+
+export default function LeftBar() {
+    const { data: session } = useSession()
+    console.log(session)
+
+  return (
+    <div className="flex flex-col h-screen p-2 bg-zinc-800 w-1/4 justify-start items-center text-center text-white">
+        {session && (
+            <>
+                <img className="h-12 w-12 rounded-full" src={session.user?.image!} alt="Profile" />
+                <p className="my-2">{session.user?.email}</p>
+                <button onClick={() => signOut()} className="bg-sky-500 rounded-md px-2 py-1 my-2">Logout</button>
+            </>
+        )}
+        
+
+    </div>
+  )
+}
