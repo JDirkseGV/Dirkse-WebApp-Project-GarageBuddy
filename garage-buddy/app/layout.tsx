699b68c127e3aa74228d42335abbb68640274c87
John <dirksej@mail.gvsu.edu>
2023-04-22 13:42:45 -0400

    added a new page with routing and page nav from the buttons

--- garage-buddy/app/layout.tsx
+++ garage-buddy/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body className="bg-zinc-700">
-        <div className="flex justify-between bg-zinc-800 py-4 rounded-md">
+        <div className="flex justify-between bg-zinc-800 py-4">
           <div className="text-white px-4">
             <a href="/" className="hover:bg-zinc-900 px-4 py-4 rounded-md">Home</a>
             <a href="/mygarage" className="hover:bg-zinc-900 px-4 py-4 rounded-md">My Garage</a>
---
+++ garage-buddy/app/mygarage/page.tsx
@@ -0,0 +1,6 @@
+export default function GaragePage() {
+    return (
+        <p className="text-white px-4">Your Vehicles: </p>
+
+    )
+}
