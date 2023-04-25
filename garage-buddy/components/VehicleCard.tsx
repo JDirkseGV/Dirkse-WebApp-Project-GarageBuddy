01cc27b3f2122da0b869b8a55527891c379684d3
John Dirkse <dirksej@mail.gvsu.edu>
2023-04-25 17:30:27 -0400

    vehicle card styling

--- garage-buddy/components/VehicleCard.tsx
+++ garage-buddy/components/VehicleCard.tsx
@@ -4,6 +4,7 @@ import { usePathname, useRouter } from "next/navigation";
 import { db } from "@/firebase";
 import { collection, doc, getDoc, query, deleteDoc } from "firebase/firestore";
 import { useSession } from "next-auth/react";
+import { TrashIcon } from '@heroicons/react/24/solid'
 
 type Properties = {
     id: string;
@@ -21,7 +22,7 @@ export default function VehicleCard({id, year, make, model}: Properties) {
     }
 
   return (
-    <div className="flex flex-col justify-between border rounded mx-2 bg-zinc-100/50">
+      <div className="flex border-zinc-800 flex-col justify-between border rounded mx-2 bg-zinc-400 border-4 hover:border-zinc-300 hover:cursor-pointer">
         <Link className="m-4" href={`/mygarage/${id}`} >
         <Image className="rounded"
             src="/../public/audi.jpg"
@@ -29,13 +30,17 @@ export default function VehicleCard({id, year, make, model}: Properties) {
             width={200}
             height={200}
             />
-            <div className="flex flex-col justify-between">
-                <p>{year}</p>
-                <p>{make}</p>
-                <p>{model}</p>
+            <div className="flex flex-col justify-between items-left text-lg">
+                <p>Year: {year}</p>
+                <p>Make: {make}</p>
+                <p>Model: {model}</p>
             </div>   
         </Link>
-        <button className="border rounded mx-16 inset-" onClick={deleteCar}>Delete</button>
+          <div className="flex border-2 border-black items-center bg-red-600 rounded w-28 h-8 justify-center m-2 hover:border-white">
+            <TrashIcon className="h-6 w-6" onClick={deleteCar}></TrashIcon> 
+            <p>delete car</p>
+        </div>
+        
     </div>
         
 
