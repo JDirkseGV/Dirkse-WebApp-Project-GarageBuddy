87d3423efffdc39b50eab5821b000f78e6c16dc7
John Dirkse <dirksej@mail.gvsu.edu>
2023-04-25 13:16:44 -0400

    styling

--- garage-buddy/app/mygarage/acceleration/page.tsx
+++ garage-buddy/app/mygarage/acceleration/page.tsx
@@ -17,6 +17,7 @@ export default function AccelerationPublic() {
     const getAcceleration = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         try {
+
             const response = await fetch('http://localhost:8000/acceleration', {
                 method: 'POST',
                 headers: {
@@ -34,19 +35,29 @@ export default function AccelerationPublic() {
     }
 
     return (
-        <div>
-            <p>Acceleration Public Model Page</p>
-            <form onSubmit={getAcceleration}>
-                <label htmlFor="weight">Weight:</label>
-                <input type="number" min="0" id="weight" value={inputs.weight} onChange={(e) => setInputs({ ...inputs, weight: parseFloat(e.target.value) })} />
-                <label htmlFor="horsepower">Horsepower:</label>
-                <input type="number" min="0" id="horsepower" value={inputs.hp} onChange={(e) => setInputs({ ...inputs, hp: parseFloat(e.target.value) })} />
-                <label htmlFor="gears">Number of Gears:</label>
-                <input type="number" min="0" id="gears" value={inputs.gears} onChange={(e) => setInputs({ ...inputs, gears: parseFloat(e.target.value) })} />
-                <button type="submit">Update Car Info</button>
+        <div className="flex flex-col items-center mt-6">
+            <h1 className="text-5xl text-white mb-6">Acceleration Public Model Page</h1>
+            <form className="mb-6 flex flex-col" onSubmit={getAcceleration}>
+                <div className="bg-sky-500 px-8 rounded-lg mb-4">
+                    <label htmlFor="weight" className="block text-2xl py-2">Curb Weight(lbs):</label>
+                    <input className="mb-6 rounded text-2xl p-1" type="number" min="0" id="weight" value={inputs.weight} onChange={(e) => setInputs({ ...inputs, weight: parseFloat(e.target.value) })} />   
+                </div>
+                <div className="bg-sky-500 px-8 rounded-lg mb-4">
+                    <label htmlFor="horsepower" className="block text-2xl py-2">Horsepower:</label>
+                    <input className="mb-6 rounded text-2xl p-1" type="number" min="0" id="horsepower" value={inputs.hp} onChange={(e) => setInputs({ ...inputs, hp: parseFloat(e.target.value) })} />  
+                </div>
+                <div className="bg-sky-500 px-8 rounded-lg mb-4">
+                    <label htmlFor="gears" className="block text-2xl py-2">Number of Gears:</label>
+                    <input className="mb-6 rounded text-2xl p-1" type="number" min="0" id="gears" value={inputs.gears} onChange={(e) => setInputs({ ...inputs, gears: parseFloat(e.target.value) })} /> 
+                </div>
+                
+                <button className="mb-6 rounded-md text-2xl bg-sky-500 p-2" type="submit">Calculate Acceleration</button>
             </form>
 
-            <p className="text-white">Predicted Acceleration: {modelPrediction.prediction}</p>
+            <div className="bg-zinc-800 rounded-lg p-4">
+                <p className="text-white text-3xl">Predicted 0-62mph: {Number((modelPrediction.prediction).toFixed(2))}</p>  
+            </div>
+            
 
         </div>
 
