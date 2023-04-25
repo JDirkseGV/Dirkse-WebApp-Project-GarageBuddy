206a97675066fcf46622a20ee75f0f77d6df695d
John Dirkse <dirksej@mail.gvsu.edu>
2023-04-25 12:39:23 -0400

    succesfully hit my ML api to have it predict, still need to fix getting the prediction response back and displaying it

--- garage-buddy/app/mygarage/acceleration/page.tsx
+++ garage-buddy/app/mygarage/acceleration/page.tsx
@@ -1,15 +1,50 @@
 'use client'
 
+import { FormEvent, useState } from "react"
+
 export default function AccelerationPublic() {
+
+    const [inputs, setInputs] = useState({
+        weight: 0,
+        hp: 0,
+        gears: 0
+    })
+
+    const [modelPrediction, setPrediction] = useState({
+        prediction: 0
+    })
+
+    const getAcceleration = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        try {
+            const response = await fetch('http://localhost:8000/acceleration', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(inputs)
+            })
+            const accelerationPrediction = await response.json()
+            setPrediction({...modelPrediction, prediction:accelerationPrediction})
+        }
+        catch (error) {
+            console.log("error : " + error)
+        }
+
+    }
 
     return (
         <div>
             <p>Acceleration Public Model Page</p>
-            <p></p>
-            <p></p>
-            <p></p>
-
-
+            <form onSubmit={getAcceleration}>
+                <label htmlFor="weight">Weight:</label>
+                <input type="number" min="0" id="weight" value={inputs.weight} onChange={(e) => setInputs({ ...inputs, weight: parseFloat(e.target.value) })} />
+                <label htmlFor="horsepower">Horsepower:</label>
+                <input type="number" min="0" id="horsepower" value={inputs.hp} onChange={(e) => setInputs({ ...inputs, hp: parseFloat(e.target.value) })} />
+                <label htmlFor="gears">Number of Gears:</label>
+                <input type="number" min="0" id="gears" value={inputs.gears} onChange={(e) => setInputs({ ...inputs, gears: parseFloat(e.target.value) })} />
+                <button type="submit">Update Car Info</button>
+            </form>
 
         </div>
 
